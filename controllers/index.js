bbf89e547086fd19e6ffdf5672c4dac04caa7678
julianbesteiro <julianbesteiro1@gmail.com>
2023-06-06 14:21:50 -0300

    create offices routes for creation, edition, update and delete

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,7 @@
 const usersController = require("./usersController");
+const officesController = require("./officesController");
 
 module.exports = {
   usersController,
+  officesController,
 };
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const users = require("./users");
+const offices = require("./offices");
 
 router.use("/users", users);
+router.use("/offices", offices);
 
 module.exports = router;
