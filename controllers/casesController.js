36d2ec1a8931485c9f2bd66c20b3da53fba88bba
inza01-git <facundoinza5@gmail.com>
2023-06-14 10:00:06 -0300

    Revert "feat: Added cant pages of filter cases glober"
    This reverts commit 794ae6d621bf4e94e4c21511e834d437a0bd23d3.

--- controllers/casesController.js
+++ controllers/casesController.js
@@ -139,26 +139,11 @@ const filterCasesGlober = async (req, res) => {
       .sort({ starting_date: -1 })
       .skip(page * casesPerPage)
       .limit(casesPerPage);
-
-    const cantCases = await Case.find({
-      $and: [
-        userId ? { user: userId } : {},
-        selectedStatus ? { status: { $in: selectedStatus } } : {},
-        startDate ? { starting_date: { $gte: startDate } } : {},
-        device && device != "all" ? { "damaged_equipment.name": device } : {},
-      ],
-    }).countDocuments();
-
     if (filteredCases.length === 0) {
       return res.status(401).json({ error: "There are no matching cases." });
     }
 
-    res
-      .status(200)
-      .json({
-        data: filteredCases,
-        countPages: Math.ceil(cantCases / casesPerPage),
-      });
+    res.status(200).json(filteredCases);
   } catch (err) {
     res.status(404).send(err);
   }
