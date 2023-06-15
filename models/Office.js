2f38ff4723f888b66c0367fbb077f3151d7add70
inza01-git <facundoinza5@gmail.com>
2023-06-15 11:05:44 -0300

    feat: Change model Office with Address

--- models/Office.js
+++ models/Office.js
@@ -6,30 +6,19 @@ const officeSchema = new mongoose.Schema({
     required: true,
   },
   address: {
-    street: {
-      type: String,
-      required: true,
-    },
-    city: {
-      type: String,
-      required: true,
-    },
-    state: {
-      type: String,
-      required: true,
-    },
-    country: {
-      type: String,
-      required: true,
-    },
+    type: String,
+    required: true,
   },
   location: {
     type: [Number],
     required: true,
   },
+  open_now: {
+    type: Boolean,
+    default: false,
+  },
   map: {
     type: String,
-    required: true,
   },
 });
 
