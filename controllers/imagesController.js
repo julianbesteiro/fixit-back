034339f529387c701fbafd5ba47aec485935d153
inza01-git <facundoinza5@gmail.com>
2023-06-16 09:57:55 -0300

    feat: Update route of create offices

--- controllers/imagesController.js
+++ controllers/imagesController.js
@@ -4,7 +4,6 @@ const { imageImgurServices } = require("../services");
 
 const imageUpdate = async (req, res) => {
   try {
-    console.log(req.body);
     const url = await imageImgurServices.setImageUrl(req.body.image);
     res.status(200).send(url);
   } catch (error) {
