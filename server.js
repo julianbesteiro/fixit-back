a0545748f6a5ab51261b31112597df950d49c5a9
julianbesteiro <julianbesteiro1@gmail.com>
2023-06-21 11:17:06 -0300

    frontend url edit

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const { connectDB } = require("./config/db");
+require("dotenv").config();
+
 //const seeder = require("./seed");
 
 const routes = require("./routes");
@@ -13,7 +15,7 @@ app.use(express.json({ limit: "50mb" }));
 // Configuración de CORS
 app.use(
   cors({
-    origin: process.env.CLIENT_URL, // Reemplaza con la URL de tu frontend en Vercel
+    origin: "https://fixit-front.vercel.app/", // Reemplaza con la URL de tu frontend en Vercel
     credentials: true,
   })
 );
@@ -24,7 +26,7 @@ app.use(cookieParser());
 
 // Configuración de seguridad de cookies
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", process.env.CLIENT_URL); // Reemplaza con la URL de tu frontend en Vercel
+  res.header("Access-Control-Allow-Origin", "https://fixit-front.vercel.app/"); // Reemplaza con la URL de tu frontend en Vercel
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header(
